809396bdfe3676476beee8f48970cab780503e44
Sergio Cutone <sergio.cutone@yp.ca>
2017-02-15 14:54:49 -0500

    Commit 2

--- git-plugin.php
+++ git-plugin.php
@@ -4,7 +4,7 @@
 Plugin Name: git-plugin
 Plugin URI:  https://google.com
 Description: Git Plugins
-Version:     1.0
+Version:     1.5
 Author:      Web Prestige
 Author URI:  https://google.com
 License:     GPL2
