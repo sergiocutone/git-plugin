ed6f24dc1c646a411fdc23276f700fe0e0db866e
Sergio Cutone <sergio.cutone@yp.ca>
2017-02-15 14:53:20 -0500

    Initial Commit

---
+++ git-plugin.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+Plugin Name: git-plugin
+Plugin URI:  https://google.com
+Description: Git Plugins
+Version:     1.0
+Author:      Web Prestige
+Author URI:  https://google.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: https://google.com
+Domain Path: https://google.co
+*/
+
+if (is_admin()) {
+	require 'plugin-update-checker/plugin-update-checker.php';
+	$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+		'https://github.com/sergiocutone/git-plugin/',
+		__FILE__,
+		'git-plugin'
+		);
+}
+
+?>
